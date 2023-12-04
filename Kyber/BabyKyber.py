43315746398c0219927a42d91112c6c01bc51797
RCW2000 <ywaitrw@gmail.com>
2023-12-03 23:33:20 -0500

    Update BabyKyber.py

--- Kyber/BabyKyber.py
+++ Kyber/BabyKyber.py
@@ -6,36 +6,50 @@ def LowPolyKeyGen():
     s1=[]
     for i in range(4):
         s1.append(random.randint(0,10)%q)
-    return np.poly1d(s1)
+    return np.polynomial.polynomial.Polynomial(s1)
 
 def BK_KeyGen():
-    s=[LowPolyKeyGen(),LowPolyKeyGen()]
-    e=[LowPolyKeyGen(),LowPolyKeyGen()]
-    A=[[0]*2]*2
+    s=[[LowPolyKeyGen()],
+       [LowPolyKeyGen()]]
+    
+    e=[[LowPolyKeyGen()],
+       [LowPolyKeyGen()]]
+    
+    A=[[0,0],
+       [0,0]]
     for i in range(2):
         for j in range(2):
             A[i][j]=LowPolyKeyGen()
     t=np.matmul(np.array(A),np.array(s))
+    
     t=np.array(t)+np.array(list(e))
-    return (np.array(list(s)),(np.array(A),t))
+    return (np.array(s)),(np.array(A),t)
 
 def BK_Enc(message,key:()):
     A,t=key
     r=(LowPolyKeyGen(),LowPolyKeyGen())
+    
     e1=(LowPolyKeyGen(),LowPolyKeyGen())
+    
     e2=(LowPolyKeyGen())
-    m=[ord(char) for char in message] 
-    for i in range(len(m)):
-        m[i]= round(q/2)*m[i]
+    
+    m=bin(message)[2:]
+    m=[int(m[i])for i in range(len(m))]
+    print(m)
+    polym=np.polynomial.polynomial.Polynomial(m)
+    polym=polym*round(q/2)
+    print(polym)
     u=np.matmul(np.transpose(A),np.array(list(r)))+np.array(list(e1))
-    v=np.matmul(np.transpose(t),np.array(list(r)))+np.array(list(e2))+np.array(m)
+    v=np.matmul(np.transpose(t),np.array(list(r)))+np.array(list(e2))+np.array([polym])
     return (u,v)
 
 def BK_Dec(ctex:(),s):
     u,v=ctex
     nm=v-np.matmul(np.transpose(s),u)
-    m=[i for i in range(nm)]
-    for i in range(nm):
+
+    print(nm)
+    m=[i for i in range(len(nm))]
+    for i in range(len(nm)):
         if round(nm[i])==round(q/2):
             m[i]=round(nm[i])/round(q/2)
         else:
@@ -46,5 +60,5 @@ def BK_Dec(ctex:(),s):
 
 #bk
 s,AT=BK_KeyGen()
-UV=BK_Enc("kybr",AT)
+UV=BK_Enc(11,AT)
 print(BK_Dec(UV,s))
